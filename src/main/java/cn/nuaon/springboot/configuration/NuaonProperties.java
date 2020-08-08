fe686cc7198d2152c7da86afebe8bb373803099b
路遥 <szhougang@gmail.com>
2020-08-08 16:18:59 +0800

    修改文件

--- src/main/java/cn/nuaon/springboot/configuration/NuaonProperties.java
+++ src/main/java/cn/nuaon/springboot/configuration/NuaonProperties.java
@@ -1,7 +1,5 @@
 package cn.nuaon.springboot.configuration;
 
-import java.util.Map;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = NuaonProperties.PREFIX)
@@ -9,14 +7,73 @@ public class NuaonProperties
 {
     public static final String PREFIX = "cn.nuaon";
 
-    private Map<String, String> info;
+    /**
+     * 站点名称
+     */
+    private String name;
 
-    public Map<String, String> getInfo()
+    /**
+     * 站点版权信息
+     */
+    private String copyriht;
+
+    /**
+     * 站点备案信息
+     */
+    private String icp;
+
+    /**
+     * 静态文件版本号
+     */
+    private String version;
+
+    /**
+     * 静态文件前缀地址
+     */
+    private String urlPrefix;
+
+    public String getName()
     {
-        return info;
+        return name;
     }
-    public void setInfo(Map<String, String> info)
+    public void setName(String name)
     {
-        this.info = info;
+        this.name = name;
+    }
+
+    public String getCopyriht()
+    {
+        return copyriht;
+    }
+    public void setCopyriht(String copyriht)
+    {
+        this.copyriht = copyriht;
+    }
+
+    public String getIcp()
+    {
+        return icp;
+    }
+    public void setIcp(String icp)
+    {
+        this.icp = icp;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+    public void setVersion(String version)
+    {
+        this.version = version;
+    }
+
+    public String getUrlPrefix()
+    {
+        return urlPrefix;
+    }
+    public void setUrlPrefix(String urlPrefix)
+    {
+        this.urlPrefix = urlPrefix;
     }
 }
